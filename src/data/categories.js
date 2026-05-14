3e3ed030799e0facaf9e3f92a637a595a5dd2bed
Nico <fnicocardozo95@gmail.com>
2026-05-14 03:01:52 -0300

    Agregar datos iniciales de categorias y productos

---
+++ src/data/categories.js
@@ -0,0 +1,27 @@
+export const categories = [
+  {
+    id: 'mates',
+    name: 'Mates',
+    description: 'Mates de calabaza, algarrobo, alpaca, aluminio e imperiales.',
+  },
+  {
+    id: 'textiles-alpaca',
+    name: 'Textiles de alpaca',
+    description: 'Chales, ruanas, buzos, camperas, medias, gorros, guantes y chulos.',
+  },
+  {
+    id: 'aguayo',
+    name: 'Aguayo',
+    description: 'Mochilas, bolsos y accesorios con estilo andino.',
+  },
+  {
+    id: 'telar-ponchos',
+    name: 'Telar y ponchos',
+    description: 'Mantas, ponchos y textiles grandes seleccionados.',
+  },
+  {
+    id: 'artesanias-madera',
+    name: 'Artesanías en madera',
+    description: 'Bateas, portarretratos y otros artículos tradicionales.',
+  },
+]
